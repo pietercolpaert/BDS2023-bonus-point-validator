a7e531cda44ff6f779aea66478e82af424635dff
Pieter Colpaert <pieter.colpaert@ugent.be>
2023-06-07 07:42:09 +0200

    Also show the first string literal attached to the person who knows me

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const rdfDereferencer = require("rdf-dereference").default;
 const process = require('process');
 const fs = require("fs");
+const N3 = require('n3');
 
 let file = 'entrypoints-example.json';
 let entrypoints = [];
@@ -28,25 +29,27 @@ let searchURLListRecursively = async function (urls) {
     history.push(url);
     try {
       const { data: triples } = await rdfDereferencer.dereference(url);
-      let nextPossibilities = [];
-      triples.on('data', (triple) => {
-        if ((triple.predicate.value === 'http://xmlns.com/foaf/0.1/knows' || triple.predicate.value === 'http://www.wikidata.org/prop/direct/P3342')) {
-          //If you found Pieter Colpaert, add it to the result list if it isn’t already
-          if (triple.object.value === 'https://pietercolpaert.be/#me' || triple.object.value === 'http://www.wikidata.org/entity/Q66736299') {
-            if(results.indexOf(triple.subject.value) === -1) {
-              results.push(triple.subject.value);
-              console.log(' * ' + triple.subject.value);
-            }
-          } else {
-            //we didn’t find Pieter Colpaert, but someone else that we’d like to follow to see whether they indicated they know Pieter Colpaert. Let’s however not follow this person if we already visited their page before.
-            if (history.indexOf(triple.object.value) === -1) {
-              nextPossibilities.push(triple.object.value);
+      const store = new N3.Store();
+      store.import(triples).on('end', () => {
+        let nextPossibilities = [];
+        for (const triple of store) {
+          if ((triple.predicate.value === 'http://xmlns.com/foaf/0.1/knows' || triple.predicate.value === 'http://www.wikidata.org/prop/direct/P3342')) {
+            //If you found Pieter Colpaert, add it to the result list if it isn’t already
+            if (triple.object.value === 'https://pietercolpaert.be/#me' || triple.object.value === 'http://www.wikidata.org/entity/Q66736299') {
+              if(results.indexOf(triple.subject.value) === -1) {
+                results.push(triple.subject.value);
+                const namesQuads = store.getQuads(triple.subject);
+                const names = namesQuads.filter((triple) => { return triple.object.termType === 'Literal' }).map( (triple) => { return triple.object.value });
+                console.log(' * ' + triple.subject.value + ' - ' + names[0]);
+              }
+            } else {
+              //we didn’t find Pieter Colpaert, but someone else that we’d like to follow to see whether they indicated they know Pieter Colpaert. Let’s however not follow this person if we already visited their page before.
+              if (history.indexOf(triple.object.value) === -1) {
+                nextPossibilities.push(triple.object.value);
+              }
             }
           }
         }
-      });
-      triples.on('error', () => { console.error('Problem parsing ' + url + ', but just continuing')});
-      triples.on('end', () => {
         searchURLListRecursively(nextPossibilities);
       });
     } catch (e) {
